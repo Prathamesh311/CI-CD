6109fe5a9fa6936cd7b22196eee70a78b2be3074
Ganesh <ganuth15@gmail.com>
2025-08-15 16:23:59 +0530

    updated new

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! updated once");
+        System.out.println("Hello World! welcome to CI CD");
     }
 }
